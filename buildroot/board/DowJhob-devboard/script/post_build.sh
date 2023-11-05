4d2b112e1db5daa84b9809103666cb9b8e6530d4
DowJhob <eulle@ya.ru>
2023-11-05 18:30:34 +0700

    fix driver
    fix gc0403 driver init array for DVP mode (get from decompile drover of dubino asleep device)
    return i2c-tools ver
    change nor flash to 32Mb
    change nor partition
    update glibc ver (for MNN )
    add overlay dir for dev

--- buildroot/board/DowJhob-devboard/script/post_build.sh
+++ buildroot/board/DowJhob-devboard/script/post_build.sh
@@ -3,7 +3,7 @@
 echo " ======================== ${BINARIES_DIR} ================================= "
 echo " ======================== ${TARGET_DIR} ================================= "
 
-mkfs.jffs2 -l -e0x10000 -s256 -d ${CONFIG_DIR}/board/DowJhob-devboard/fs-overlay/home -o ${BINARIES_DIR}/home.jffs2
+mkfs.jffs2 -l -e0x10000 -s256 -d ${CONFIG_DIR}/board/DowJhob-devboard/home -o ${BINARIES_DIR}/home.jffs2
 
 LINE='/dev/mtdblock4     /home           jffs2   defaults        0       2'
 FILE=${TARGET_DIR}/etc/fstab
